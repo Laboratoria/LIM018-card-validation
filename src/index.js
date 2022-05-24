ab7d40f51deca9011b6db46462950031ad5e809a
Myriam <myriam.rivera.santiago@gmail.com>
2022-05-23 22:43:08 -0500

    se agrega interfaz página principal y página de compra

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-import validator from './validator.js';
+import validator from "./validator.js";
 
 console.log(validator);
+
+/* Muestra pantalla 2 */
+const boton = document.getElementById("btn-comprar");
+
+function comprar() {
+  document.getElementById("paso1").style.display = "none";
+  document.getElementById("paso2").style.display = "block";
+}
+
+boton.addEventListener("click", comprar);
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,21 @@
+const creditCardNumberExample = "1234567890";
 const validator = {
-  // ...
+  isValid: (creditCardNumber) => {
+    const length = creditCardNumber.length;
+    let count = 0;
+
+    for (let i = 0; i < length; i++) {
+      let currentDigit = parseInt(creditCardNumber[i]);
+      if ((i + 1) % 2 == 0) {
+        if ((currentDigit = currentDigit * 2) > 9)
+          currentDigit = currentDigit - 9;
+        count = count + currentDigit;
+      }
+      return count % 10 === 0;
+    }
+  },
+
 };
+console.log("validar", validator.isValid(creditCardNumberExample));
 
 export default validator;
