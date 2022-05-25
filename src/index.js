bdbe646ab0a0177deb95337d0f17b8970375aca0
Jackeline Puruaya <jaishackeline@gmail.com>
2022-05-25 10:54:03 -0500

    Añadí 2 imágenes de APRIORI

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import validator from './validator.js';
 validator.isValid(4083952015263);
 validator.isValid(79927398713);
 validator.isValid(1234567890);
-validator.maskify(1234456623);
+validator.maskify(4556364607935616);
+validator.maskify(1);
+validator.maskify("hello world");
 
 
 
--- src/validator.js
+++ src/validator.js
@@ -8,23 +8,31 @@ const validator = {
             //double position par
             digits = digits.reverse().map((digit,i)=> i%2===0?digit*2:digit);
             
-      console.log(digits)
+      //console.log(digits)
     } 
     
     //fix the double digits
     digits = digits.map(digit=> digit>9 ? digit-9: digit);
-    console.log(digits)
+    //console.log(digits)
     //sume all digits
       const sum= digits.reduce((acumulation,digit)=> acumulation== digit, 0);
     return sum%10===0;
   
     
   },
-  maskify : ()=>{
+  maskify : (n)=>{
+  let digits = n.toString();
+  console.log(digits);
+    if(digits.length>1){
+    
+    console.log(digits);
+  } 
+  
+    
 // recorrer el array con map()
 // filtrar con filter()las posiciones 0 al 12 del arreglo
 // Reemplazar con replace() con #
-// Hemos llegado hasta aquí
+// Llegué hasta aquí
   },
   
   };
