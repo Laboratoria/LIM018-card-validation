19fdbc2665811413eee3fd8a864efcd4815a54b1
Roxann <lopezbedonroxana@gmail.com>
2022-05-28 19:14:33 -0500

    trabajo al 90%

--- src/index.js
+++ src/index.js
@@ -1,63 +1,47 @@
-//import validator from './validator.js';
+import validator from './validator.js';
 
-//console.log(validator);
+// Obtener los datos de los inputs
+
+const nombres = document.querySelector("#nombre");
+const numero = document.querySelector("#numero");
+const fecha = document.querySelector("#fecha");
+const cvv = document.querySelector("#cvv");
 
 
-// Es el numero de la tarjeta
-const num = '4568259713645695';
+const pago = document.querySelector("#pago");
 
-const validarTarjeta = numCard => {
-	// Variables que usaremos
-  let array = [], aux = 1, total = 0, validar = false, inicio = 0, salida = '';
-      
-  const resultado = {
-    validar: validar,
-    salida: salida
-  }
+pago.addEventListener('click', () => {
   
-	// Validamos que el numero sea exactamente 16 caracteres   
-  if(numCard.length !== 16) return resultado;
-  
-	// Recibimos el numero de la tarjeta en un String luego lo convertimos a un arreglo y lo invertimos 
-  array = num.split('').reverse();
-  
-	// Usamos la funcion map para crear un nuevo arreglo y recorrer el arreglo principal para convertir los elementos a valor numerico
-  array = array.map( e => parseInt(e));
+   if(nombres.value.length === 0 ){
+     alert('Debe ingresar su nombre!')
+     return;
+   } 
 
-	// Multiplicamos por 2 a los numeros que se encuentre en las posiciones pares del arreglo y si el valor al multiplicar fuera mayor o igual a 10 entonces sumar sus digitos   
-  array = array.map( e => {
-    if(aux % 2 === 0){
-      e = e * 2;
-      if(e >= 10){
-        const newArray = e.toString().split('');
-        e = parseInt(newArray[0]) + parseInt(newArray[1]);
-      }
-    }
-    aux++;
-    return e;
-  });
+   if(numero.value.length === 0){
+     alert('Ingrese su numero de tarjeta!')
+     return;
+   }
+
+   if(fecha.value.length === 0){
+     alert('Ingrese su fecha!')
+     return;
+   }
+
+   if(cvv.value.length !== 3){
+     alert('CVV Invalido!')
+     cvv.value = ''
+     return;
+   }
+
+  const validar = validator.validarCard(numero.value);
   
-	// Sumar el total de todos los numeros de arreglo
-  array.forEach( e => total += e);
-  
-	// Validar si a la suma total su modulo de 10 es igual sera valido  
-  validar = total % 10 === 0? true : false; 
-  resultado.validar = validar;
-  
-	// Imprimir solos los ultimos 4 digitos de la tarjeta
-    array= num.split("");
-  array.forEach( e => {
-    if(array.length - 4 > inicio){
-      salida += '*';
-    }
-    else{
-      salida += e;
-    }
-    inicio++;
-  });
-  resultado.salida = salida;
-  
-  return resultado;
-}
-const salida = validarTarjeta(num);
-console.log(salida);
+   if(validar) {
+     const tarjetaMask = validator.tarjetaMask(numero.value);
+     alert(`Pago procesado! \n${tarjetaMask}`)
+   }
+   else{
+     alert('Tarjeta no valida!')
+   }
+
+
+})
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,54 @@
+const validarCard = (numCard) =>{
+
+  if(isNaN(Number(numCard))) return false;
+  
+  if(numCard.length !== 16) return false;
+
+  let array = [...numCard].reverse();
+
+  array = array.map( e => Number(e));
+
+  let aux = 1;
+
+  array = array.map( e => {
+    if(aux % 2 === 0){
+      e = e * 2;
+      if(e >= 10){
+        e -= 9
+      }
+    }
+    aux++;
+    return e;
+  })
+
+  let total = 0;
+  array.forEach( e => total += e)
+
+  return (total % 10 === 0) ? true : false
+}
+
+const tarjetaMask = (numCard) => {
+  let array = [...numCard];
+  let salida = "";
+
+  let aux = 1;
+
+  for(let a of array){
+    if(aux < 13){
+      salida += "*";
+    }
+    else{
+      salida += a
+    }
+    aux++;
+  }
+
+  return salida
+}
+
 const validator = {
-  // ...
+  validarCard,
+  tarjetaMask
 };
 
 export default validator;
