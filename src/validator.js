cda09029eb4243400abe85b2dbcfa6ea8dc922d0
Jackeline Puruaya <jaishackeline@gmail.com>
2022-06-02 13:01:06 -0500

    arreglando funciones

--- src/validator.js
+++ src/validator.js
@@ -2,40 +2,35 @@ const validator = {
   
   isValid : (n)=> {
     let digits = n.toString().split('').map(Number);
-    //console.log(typeof digits)
+    
+       //console.log(typeof digits)
       // condition 
     if (digits.length>15){
-      
-            //double position par
-            digits = digits.reverse().map((digit,i)=> i%2===0?digit*2:digit);
-            
+      //double position par
+      digits = digits.reverse().map((digit,i)=> i%2===0?digit*2:digit);
       //console.log(digits)
     } 
-    
     //fix the double digits
     digits = digits.map(digit=> digit>9 ? digit-9: digit);
     //console.log(digits)
-    //sume all digits
-      const sum= digits.reduce((acumulation,digit)=> acumulation== digit, 0);
+    //all digits
+    const sum= digits.reduce((acumulation,digit)=> acumulation== digit, 0);
     return sum%10===0;
-  
-    
+      
   },
   maskify : (n)=>{
     let digits= Array.from(n);
+    
     digits.map((acc, i)=>i<digits.length-4?digits[i]='#':digits)
 
       //for (let i = 0; i< digits.length-4;i++){
       //digits[i] = '#';
        
     //}
-     return digits.toString().replace(/,/g,"");
-  
-        
-
+    return digits.toString().replace(/,/g,"");
   },
   
-  };
+};
 
 
 export default validator;
