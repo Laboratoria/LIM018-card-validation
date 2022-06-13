7d019bf12ebc019a8f84e12e5140db00881927de
Jackeline Puruaya <jaishackeline@gmail.com>
2022-06-13 14:56:53 -0500

    isValid() funcionando correctamente

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import validator from "./validator.js";
 let title = document.getElementById("title");
 let reservation = document.getElementById("reservation");
-let correct = document.getElementById("correct");
-let incorrect = document.getElementById("incorrect");
+//let correct = document.getElementById("correct");
+//let incorrect = document.getElementById("incorrect");
 let btn_yes = document.getElementById("btn_yes");
 //let form_f = document.getElementById("form_f");
 let form_s = document.getElementById("form_s");
+let numberCard = document.getElementById("numberCard");
 let btn_validator = document.getElementById("input_validation")
 //variable boton validar
 // eslint-disable-next-line no-console
@@ -19,23 +20,33 @@ btn_yes.addEventListener("click", () => {
   
  title.style.display = "none";
   reservation.style.display = "none";
-  correct.style.display = "none";
-  incorrect.style.display = "none";
+  //correct.style.display = "none";
+  //incorrect.style.display = "none";
 
   reservation.textContent = form_s;
 
   // eslint-disable-next-line no-console
-  console.log(form_s);
+  //console.log(form_s);
 }
 
 );
 
 btn_validator.addEventListener("click", (event) => {
   event.preventDefault()
-  validator.isValid("hola");
-  let bonito = document.getElementById("bonito");
-  bonito.innerHTML = "<h3> hola </h3>"
+  let inputNumberCard = numberCard.value;
+  if(validator.isValid(inputNumberCard)){
+    alert("correcto");
+  } else {
+    alert("incorrecto");
+  }
+  //let form_s = document.getElementById("form_s");
+  
+ /*  validator.isValid("hola"); */
+/*   let bonito = document.getElementById("bonito");
+  bonito.innerHTML = "<h3> hola </h3>" */
   //evitar recargar página investigar
+
+  
 })
 
 //alert para true or false 
--- src/validator.js
+++ src/validator.js
@@ -1,22 +1,27 @@
 const validator = {
   isValid: (n) => {
-    let digits = n.toString().split("").map(Number);
-
+    let digits = n.split("").map(Number);
+  
     //console.log(typeof digits)
     // condition
     if (digits.length > 10) {
       //double position par
       digits = digits
         .reverse()
-        .map((digit, i) => (i % 2 === 0 ? digit * 2 : digit));
-      //console.log(digits)
-    }
+        .map((digit, i) => (i % 2 === 1? digit * 2 : digit));
+     
+    }  
+    
     //fix the double digits
     digits = digits.map((digit) => (digit > 9 ? digit - 9 : digit));
     //console.log(digits)
     //all digits
-    const sum = digits.reduce((acumulation, digit) => acumulation == digit, 0);
+    const sum = digits.reduce((acumulation, digit) => acumulation += digit);
+    //console.log(sum);
+    
     return sum % 10 === 0;
+  
+      
   },
   maskify: (n) => {
     let digits = Array.from(n);
