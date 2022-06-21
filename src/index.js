1fa5c112c47e20cc6bb812cef95208caee56811d
Jackeline Puruaya <jaishackeline@gmail.com>
2022-06-21 07:30:39 -0500

    Js funcionando bien

--- src/index.js
+++ src/index.js
@@ -1,97 +1,82 @@
 import validator from "./validator.js";
 //secciones 
-const title = document.getElementById("title");
-const reservation = document.getElementById("reservation");
+const reservation_s = document.getElementById("reservation_s");
 const form_s = document.getElementById("form_s");
+const pay_s = document.getElementById("pay_s");
+const end_s =document.getElementById("end_s");
 //botones para los eventos
-const pay = document.getElementById("pay");
-const end =document.getElementById("end");
+
 const btn_yes = document.getElementById("btn_yes");
+const btn_validator = document.getElementById("input_validation");
 const btn_pay = document.getElementById("btn_pay");
 const btn_end = document.getElementById("btn_end");
 //inputs
 const numberCard = document.getElementById("numberCard");
-const names = document.getElementById("names");
-const expiration = document.getElementById("expiration");
 const ccv = document.getElementById("ccv");
-const btn_validator = document.getElementById("input_validation")
+//mensajes para el usuario
 const message = document.getElementById("message");
 
 // ocultar las secciones que no necesito mostrar al inicio
  form_s.style.display= "none";
- pay.style.display= "none";
- end.style.display= "none";
-
-
+ pay_s.style.display= "none";
+ end_s.style.display= "none";
+ 
  //evento que me permite pasar a la siguiente vista
 btn_yes.addEventListener("click", () => {
-  title.style.display = "none";
-  reservation.style.display = "none";
+  reservation_s.style.display = "none";
   form_s.style.display="inline-flex";
  }
 
 );
-
-//evento que me permite pasar a validar mis inputs
+//evento que me permite pasar a validar el número de tarjeta
 btn_validator.addEventListener("click", (event) => {
   event.preventDefault()
   let inputNumberCard = numberCard.value;
-  let inputName = 
   let muskify= validator.maskify(inputNumberCard);
   
-  if (inputNumberCard === "" && ){
+  if (inputNumberCard === ""){
       message.innerText = "Ingrese un número de tarjeta";
-  }
+     //¿Por qué estoy usando inner.Text y no inner.HTML?
+  } 
   else {
    if (validator.isValid(inputNumberCard)){
-    //document.getElementById("numberCard").textContent = validator.maskify(muskify);
-    //alert(muskify);
-   //form_s.style.display = "none"; // display: block
-  //correct.style.display = "none";
-  //incorrect.style.display = "none";
-//visibility: hidden : visible
-
-    //form_s.textContent = correct;
    document.getElementById("numberCard").value = muskify;
-   //correct.style.display = "inline-flex";
-    message.innerHTML = "Tajerta válida"; 
+
+    message.innerHTML = "Tarjeta válida"; 
   } else {
-    //form_s.style.display = "none";
-    //correct.style.display = "none";
-  //incorrect.style.display = "none";
+    
     message.innerText = "Tarjeta inválida";
-    //form_s.textContent = incorrect;
-    //alert("incorrecto");
+   
   }
   }
-  //console.log(muskify);
-  //validator.maskify(inputNumberCard);
-  //let form_s = document.getElementById("form_s");
-  
- /*  validator.isValid("hola"); */
-/*   let bonito = document.getElementById("bonito");
-  bonito.innerHTML = "<h3> hola </h3>" */
-  //evitar recargar página investigar
 })
 
-/* //boton pagar: sección válida la compra
-let refresh_btn_incorrect = document.getElementById('btn_incorrect');
-refresh_btn_incorrect.addEventListener('click', () => {
-            location.reload();
-                   
-});  */
+//Evento para hacer el pago
+btn_pay.addEventListener("click",(event)=> {
+  event.preventDefault();
+  let ipt_c = ccv.value;
+  if(ipt_c!=""){
+    form_s.style.display= "none";
+    reservation_s.style.display= "none";
+    end_s.style.display= "none";
+    pay_s.style.display="inline-flex";
+    
+  } else{
+    message.innerHTML= "Complete todos los campos vacíos";
+  }
+  
 
-//darle estilos a los botones desde CSS 
+})
 
-let refresh_btn_correct = document.getElementById('btn_correct');
-refresh_btn_correct.addEventListener('click',
- () => {
-            location.reload();
-});
-//alert para true or false 
-// mensaje
-
-// crear un elemento de HTML para mostrar desde Js 
+//evento para finalizar
+btn_end.addEventListener("click",(event)=> {
+  event.preventDefault();
+  
+    form_s.style.display= "none";
+    pay_s.style.display= "none";
+    reservation_s.style.display= "none";
+    end_s.style.display= "inline-flex";
+})
 
 //parent.getElementByTagName();
 //parent.getElementByClassName();
