00e40fddbdcf958c484c61dc4b0978e0d9c0a707
Jackeline Puruaya <jaishackeline@gmail.com>
2022-06-22 13:22:03 -0500

    terminado, antes del proyect Feedback

--- src/validator.js
+++ src/validator.js
@@ -3,22 +3,23 @@ const validator = {
     let digits = n.split("").map(Number);
 
     //console.log(typeof digits)
-    // condition
+    //condición general
     if (digits.length > 10) {
-      //double position par
+      //duplicar posición par
       digits = digits
         .reverse()
         .map((digit, i) => (i % 2 === 1 ? digit * 2 : digit));
     }
-    //fix the double digits
+    //sumar los dígitos duplicados con 2 dígitos
     digits = digits.map((digit) => (digit > 9 ? digit - 9 : digit));
     //console.log(digits)
-    //all digits
+    //sumar todos los dígitos
     const sum = digits.reduce((acumulation, digit) => (acumulation += digit));
     //console.log(sum);
     return sum % 10 === 0;
   },
   maskify: (n) => {
+    //convierto un string en array
     let digits = Array.from(n);
 
     digits.map((acc, i) =>
