c4bccd6287eb4657ca4214e62fed7f386052d459
LeylaSolange <leylagamarra.movistar@gmail.com>
2022-06-23 12:44:49 -0500

    validatortarjeta

--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,42 @@
 const validator = {
-  // ...
+  isValid: (creditCardNumber)=>{
+ let strCreditcard =creditCardNumber.split("").reverse().join("");
+let arrCreditcard = Array.from(strCreditcard);
+let totalPar = 0;
+let totalimPar = 0;
+
+  for(var i=0; i < arrCreditcard.length; i++){
+    if(i%2 ===1){
+      if(arrCreditcard[i]*2>9){
+        totalPar += arrCreditcard[i]*2-9;
+      }
+      else {
+        totalPar += arrCreditcard[i]*2;
+      }
+    }
+    else {
+      totalimPar += parseInt(arrCreditcard[i]);
+      
+    }
+  
+  }
+ 
+  if ((totalPar + totalimPar)%10 ===0){
+   return true;
+  }
+  else{
+    return false; 
+  }
+  },
+
+  maskify:(valorCreditcard)=>{
+    let mascara = valorCreditcard.slice(0,-4).replace(/\w/g,"#");
+    let numeroslibres=valorCreditcard.slice(-4);
+    let maskCreditcard=mascara + numeroslibres;
+  
+    return maskCreditcard ;
+  }
+  
 };
 
 export default validator;
